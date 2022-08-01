653c207f6f22cb99fde66e9b551e24a997e1c2c4
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-01 21:47:36 +0530

    app heading created

--- src/Design/Heading.js
+++ src/Design/Heading.js
@@ -3,7 +3,18 @@ import React from "react";
 export default function Heading() {
   return (
     <div>
-      <p>Todo Application</p>
+      <p
+        style={{
+          margin: "0px",
+          padding: "15px 30px",
+          fontWeight: "bold",
+          fontSize: "25px",
+          background: "#1969c6",
+          color: "white",
+        }}
+      >
+        Todo Application
+      </p>
     </div>
   );
 }
